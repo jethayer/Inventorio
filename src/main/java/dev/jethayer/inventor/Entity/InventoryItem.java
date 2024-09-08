5a74d450b730c1f6c6630543711275230634f362
jethayer <jethayer00@gmail.com>
2024-09-07 19:21:47 -0700

    Create Inventory item class

---
+++ src/main/java/dev/jethayer/inventor/Entity/InventoryItem.java
@@ -0,0 +1,25 @@
+package dev.jethayer.inventor.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "inventory_items")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class InventoryItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    @Column
+    private double price;
+}
