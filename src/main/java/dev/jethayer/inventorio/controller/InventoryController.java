e575485cfdbeffa0fffa78a08422b51955af1eab
jethayer <jethayer00@gmail.com>
2024-09-08 20:34:21 -0700

    Refactored naming conventions and imports

--- src/main/java/dev/jethayer/inventorio/controller/InventoryController.java
+++ src/main/java/dev/jethayer/inventorio/controller/InventoryController.java
@@ -2,14 +2,7 @@ package dev.jethayer.inventorio.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import dev.jethayer.inventorio.model.InventoryItem;
 import dev.jethayer.inventorio.service.InventoryService;
