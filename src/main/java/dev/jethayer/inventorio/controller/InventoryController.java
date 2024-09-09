a14dd4a3224e1c2064ac4aea6244739199d1210c
jethayer <jethayer00@gmail.com>
2024-09-08 20:24:38 -0700

    Crud operations for Inventory controller

---
+++ src/main/java/dev/jethayer/inventorio/controller/InventoryController.java
@@ -0,0 +1,53 @@
+package dev.jethayer.inventorio.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.jethayer.inventorio.model.InventoryItem;
+import dev.jethayer.inventorio.service.InventoryService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping
+public class InventoryController {
+    
+    @Autowired
+    private InventoryService service;
+
+    @GetMapping
+    public List<InventoryItem> getAllItems() {
+        return service.getAllItems();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<InventoryItem> getItemById(@PathVariable Long id) {
+        return service.getItemById(id)
+                        .map(ResponseEntity::ok)
+                        .orElse(ResponseEntity.notFound().build());   
+    }
+
+    @PostMapping
+    public InventoryItem createItem(@RequestBody InventoryItem item) {
+        return service.saveItem(item);
+    }
+
+    @PutMapping("/id")
+    public ResponseEntity<InventoryItem> updateItem(@PathVariable Long id, @RequestBody InventoryItem item) {
+        return ResponseEntity.ok(service.updateItem(id, item));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteItem(@PathVariable Long id) {
+        service.deleteItem(id);
+        return ResponseEntity.noContent().build();
+    }
+}
