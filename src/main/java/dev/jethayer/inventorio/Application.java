3bd4543909c0abde938953002d0ddf6922ed197d
jethayer <jethayer00@gmail.com>
2024-09-08 19:31:51 -0700

    Create inventory service to get save update and delete items

--- src/main/java/dev/jethayer/inventorio/Application.java
+++ src/main/java/dev/jethayer/inventorio/Application.java
@@ -1,4 +1,4 @@
-package dev.jethayer.inventor;
+package dev.jethayer.inventorio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/dev/jethayer/inventorio/model/User.java
+++ src/main/java/dev/jethayer/inventorio/model/User.java
@@ -1,4 +1,4 @@
-package dev.jethayer.inventor.model;
+package dev.jethayer.inventorio.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
